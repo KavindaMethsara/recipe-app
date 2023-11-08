68d4bd41e430a09e81404461833abaa35ea7e699
KavindaMethsara <methsara31@gmail.com>
2023-11-08 23:45:09 +0530

    First version completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import "../assets/css/styles.css"
 
 const Home = () => {
   const [recipes, setRecipes] = useState([]);
@@ -23,44 +24,33 @@ const Home = () => {
       });
   }, []);
   return (
-    <div className="p-4">
-      <div className="flex justify-between items-center">
-        <h1 className="text-3xl my-8">Recipe List</h1>
-        <Link to="/recipes/add">
-          <MdOutlineAddBox className="text-sky-800 text-4xl" />
-        </Link>
-      </div>
+    <div className="p-4 main-page">
+      <div className="flex justify-between items-center"></div>
       {loading ? (
         <Spinner />
       ) : (
-        <table className="w-full border-separate border-spacing-2">
+        <table className="w-full border-spacing-2">
           <thead>
             <tr>
-              <th className="border border-slate-600 rounded-md">No</th>
-              <th className="border border-slate-600 rounded-md">
-                Recipe Name
-              </th>
-              <th className="border border-slate-600 rounded-md">Operations</th>
+              <th className="border">No</th>
+              <th className="border">Recipe Name</th>
+              <th className="border">Operations</th>
             </tr>
           </thead>
           <tbody>
             {recipes.map((recipe, index) => (
               <tr key={recipe._id} className="h-8">
-                <td className="border border-slate-700 rounded-md text-center">
-                  {index + 1}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center">
-                  {recipe.name}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center">
+                <td className="border text-center">{index + 1}</td>
+                <td className="border text-center">{recipe.name}</td>
+                <td className="border text-center">
                   <div className="flex justify-center gap-x-4">
-                    <Link to={`/recipes/details/${recipe._id}`}>
+                    <Link to={`/recipes/details/${recipe?._id}`}>
                       <BsInfoCircle className="text-2xl text-green-800" />
                     </Link>
-                    <Link to={`/recipes/edit/${recipe._id}`}>
+                    <Link to={`/recipes/edit/${recipe?._id}`}>
                       <AiOutlineEdit className="text-2xl text-yellow-800" />
                     </Link>
-                    <Link to={`/recipes/delete/${recipe._id}`}>
+                    <Link to={`/recipes/delete/${recipe?._id}`}>
                       <MdOutlineDelete className="text-2xl text-red-800" />
                     </Link>
                   </div>
@@ -74,4 +64,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Home;
